657573fc014a01752b847d253529fc49dee6916d
hall <1425795337@qq.com>
2018-11-26 18:54:01 +0800

    编写界面完成，但是没有完成提交功能

--- pmd_article/urls.py
+++ pmd_article/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     re_path('^$', views.index),
     re_path('^p/(\d+)$', views.detail),
+    re_path('^w/(\d+)$', views.write),
+    re_path('^save(\d+)$', views.save)
 ]
--- pmd_article/views.py
+++ pmd_article/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 from .models import *
 import markdown
 # Create your views here.
 
+
 def index(request):
     """
     主页，展示文章列表
-    :param request:
-    :return:
     """
     article_list = ArticleInfo.objects.all().order_by("-id")
     # introduction = article_list
@@ -22,8 +21,6 @@ def index(request):
 def detail(request, id):
     """
     文章的详情页
-    :param request:
-    :return:
     """
     article = ArticleInfo.objects.filter(id=id)[0]
     html = markdown.markdown(article.a_content)
@@ -33,3 +30,33 @@ def detail(request, id):
         'html': html
     }
     return render(request, 'pmd_article/detail.html', context)
+
+
+def write(request, id):
+    """
+    id=0: 新增文章
+    id=其他: 修改文章
+    """
+    if id == 0:
+        article = ArticleInfo.objects.filter(id=id)[0]
+        html = markdown.markdown(article.a_content)
+    else:
+        article = ArticleInfo.objects.filter(id=id)[0]
+        html = markdown.markdown(article.a_content)
+    context = {
+        'article': article,
+        'html': html,
+    }
+    return render(request, 'pmd_article/write.html', context)
+
+
+def save(request, id):
+    """
+    修改文章
+    """
+    txt = request.POST['article']
+    txt = markdown.markdown(txt)
+    data = {
+        'data': txt,
+    }
+    return JsonResponse(data)
